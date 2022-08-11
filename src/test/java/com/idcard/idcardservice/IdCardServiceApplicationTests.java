d8f22e19626a98c30fab6e06325860394d27f745
Vifieh Ruth <vifieh.ruth@jan-it-solutions.com>
2022-08-11 13:23:34 +0100

    initial commit

---
+++ src/test/java/com/idcard/idcardservice/IdCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idcard.idcardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdCardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
